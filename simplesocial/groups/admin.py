f115aa883b46bebaa1ed8512d813eef971821572
FahimKamal <fahimkamal63@gmail.com>
2020-05-26 15:29:30 +0600

    part 8 complete.

--- simplesocial/groups/admin.py
+++ simplesocial/groups/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+
+
+class GroupMemeberInline(admin.TabularInline):
+    model = models.GroupMember
+
+
+admin.site.register(models.Group)
