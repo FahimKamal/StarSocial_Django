5b822abb0f78cbe0159ac44f3d83555eb9f47533
FahimKamal <fahimkamal63@gmail.com>
2020-05-26 11:34:01 +0600

    part 8 complete.

--- simplesocial/posts/models.py
+++ simplesocial/posts/models.py
@@ -6,7 +6,6 @@ import misaka
 from groups.models import Group
 from django.contrib.auth import get_user_model
 
-
 # Create your models here.
 User = get_user_model()
 
@@ -24,10 +23,10 @@ class Post(models.Model):
     def save(self, *args, **kwargs):
         self.message_html = misaka.html(self.message)
         super().save(*args, **kwargs)
-        
+
     def get_absolute_url(self):
-        return reverse('posts:single', kwargs={'username':self.user.username,
-                                               'pk':self.pk})
+        return reverse('posts:single', kwargs={'username': self.user.username,
+                                               'pk': self.pk})
 
     class Meta:
         ordering = ['-created_at']
